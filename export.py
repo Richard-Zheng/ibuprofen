785b0decf9d39b8502a6cd3fa732502a0f5847f5
Richard-Zheng <yuqiu36@live.com>
2021-04-04 19:20:34 +0800

    add login

--- export.py
+++ export.py
@@ -45,14 +45,19 @@ def generate_user_class_html(user_class):
             a.meta(charset="utf-8")
             a.title(_t=user_class.name)
         with a.body():
-            for key in index_to_subject_name:
-                with a.label():
-                    a.input(checked='checked', name='category', onchange='onSubjectCheckboxChange()', type='checkbox',
-                            value=key)
-                    a(index_to_subject_name[key])
-            with a.p():
-                a.button(onclick='changeAll(true)', type='button', _t='全选')
-                a.button(onclick='changeAll(false)', type='button', _t='全不选')
+            with a.div(id='subject-filter'):
+                for key in index_to_subject_name:
+                    with a.label():
+                        a.input(checked='checked', name='category', onchange='onSubjectCheckboxChange()', type='checkbox',
+                                value=key)
+                        a(index_to_subject_name[key])
+                with a.p():
+                    a.button(onclick='changeAll(true)', type='button', _t='全选')
+                    a.button(onclick='changeAll(false)', type='button', _t='全不选')
+            with a.div(id='user-login'):
+                a.label(for_='user-id', _t='用户名:')
+                a.input(id='user-id', type='text')
+                a.input(onclick='login()', type='submit', value='登录')
             with a.ul():
                 for record in reversed(user_class.lesson_schedules):
                     if not 'title' in record:
@@ -66,7 +71,7 @@ def generate_user_class_html(user_class):
                             for resource in record['file_resources']:
                                 if not 'fileURI' in resource:
                                     continue
-                                with a.li():
+                                with a.li(klass='resource', **{'data-guid': resource['guid']}):
                                     with a.a(href=resource['fileURI']):
                                         a(resource['title'])
                                     if resource['ext'] in pdf_convertable_exts:
--- site/scripts/user_class_page_script.js
+++ site/scripts/user_class_page_script.js
@@ -1,3 +1,45 @@
+var userGuid = "ffffffffffffffffffffffffffffffff";
+const soapUrl = `${window.location.protocol}//${window.location.host}/wmexam/wmstudyservice.WSDL`;
+
+function getSoapRequestBody(action, params) {
+    var res = `<v:Envelope xmlns:v="http://schemas.xmlsoap.org/soap/envelope/" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:d="http://www.w3.org/2001/XMLSchema" xmlns:c="http://schemas.xmlsoap.org/soap/encoding/">
+    <v:Header/>
+    <v:Body>
+        <${action} xmlns="http://webservice.myi.cn/wmstudyservice/wsdl/" id="o0" c:root="1">`;
+    for (let key in params) {
+        res += `
+            <${key} i:type="d:${typeof params[key]}">${params[key]}</${key}>`
+    }
+    res += `
+        </${action}>
+    </v:Body></v:Envelope>`;
+    return res;
+}
+
+function getSoapRequest(soapUrl, action, params, onload) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", soapUrl);
+    xhr.withCredentials = true;
+    xhr.setRequestHeader('User-Agent', 'ksoap2-android/2.6.0+');
+    xhr.setRequestHeader('SOAPAction', `http://webservice.myi.cn/wmstudyservice/wsdl/${action}`);
+    xhr.setRequestHeader('Content-Type', 'text/xml;charset=utf-8');
+    xhr.setRequestHeader('Cookie', `userguid=${userGuid};username=paduser;usergroupguid=ffffffffffffffffffffffffffffffff`);
+    xhr.setRequestHeader('Accept-Encoding', 'gzip')
+    xhr.responseType = "document";
+    xhr.addEventListener("load", onload)
+    xhr.send(getSoapRequestBody(action, params));
+    return xhr;
+}
+
+function login() {
+    console.log(soapUrl)
+    userId = document.getElementById("user-id").value;
+    let xhr = getSoapRequest(soapUrl, "UsersGetUserGUID", {"lpszUserName": userId}, () => {
+        userGuid = xhr.responseXML.getElementsByTagName("AS:szUserGUID")[0].innerHTML;
+        alert("登录成功 " + userGuid);
+    });
+}
+
 function changeAll(b) {
     var items = document.getElementsByName("category");
     for (let i of items) {
